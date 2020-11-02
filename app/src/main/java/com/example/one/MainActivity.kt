a921d321b0ed32a1bf36c401897945e09b6a4a40
Антон Логинов <loginov.anton.1999@gmail.com>
2020-11-02 19:35:28 +0300

    Change page structure

--- app/src/main/java/com/example/one/MainActivity.kt
+++ app/src/main/java/com/example/one/MainActivity.kt
@@ -1,73 +1,24 @@
 package com.example.one
 
-//import android.R
-
 import android.content.Intent
-import android.content.res.Configuration
-import android.hardware.Camera
 import android.os.Bundle
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.widget.Button
-import android.view.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.alphamovie.lib.AlphaMovieView
 import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.CaptureActivity
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
 
 
-class MainActivity :AppCompatActivity(),
-    SurfaceHolder.Callback, View.OnClickListener, Camera.PictureCallback,
-    Camera.PreviewCallback, Camera.AutoFocusCallback {
-
-    private var isMovie = false
-    private var alphaMovieView: AlphaMovieView? = null
-    private var camera: Camera? = null
-    private var surfaceHolder: SurfaceHolder? = null
-    private var preview: SurfaceView? = null
-    private val shotBtn: Button? = null
-
+class MainActivity :AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        alphaMovieView = findViewById(R.id.video_player)
-        alphaMovieView!!.setVideoFromAssets("blender_V20001-0168.mp4")
-        alphaMovieView!!.setOnVideoEndedListener { Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show() }
-
-        // если хотим, чтобы приложение постоянно имело портретную ориентацию
-//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-        // если хотим, чтобы приложение было полноэкранным
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        // и без заголовка
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        // наше SurfaceView имеет имя SurfaceViewCamera
-        preview = findViewById(R.id.SurfaceViewCamera);
-
-        surfaceHolder = preview!!.holder;
-        surfaceHolder!!.addCallback(this);
-        surfaceHolder!!.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
         scanQRCode()
     }
 
-//    override fun onResume() {
-//        super.onResume()
-//        alphaMovieView!!.onResume()
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        alphaMovieView!!.onPause()
-//    }
+    override fun onStart() {
+        super.onStart()
+    }
 
     private fun scanQRCode(){
         val integrator = IntentIntegrator(this).apply {
@@ -77,123 +28,34 @@ class MainActivity :AppCompatActivity(),
             setPrompt("Scanning Code")
         }
         integrator.initiateScan()
+
     }
 
     // Get the results:
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
-            if (result.contents == null) Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            if (result.contents == null){
+
+//                return
+//                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+//                onBackPressed()
+            }
             else {
-                if(result.contents == "Музей Гомельского государственного университета имени Франциска Скорины")
-                {
-                    isMovie = true
-                    resume()
-                }else {
+                if(result.contents.startsWith("Музей")) {
+                    val intent = Intent(this, VideoActivity::class.java)
+                    startActivity(intent)
+                    return
+                }
+                else{
                     Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-                    scanQRCode()
+//                return
                 }
             }
         } else {
+//            return
+//            Toast.makeText(this, "Cancelled2", Toast.LENGTH_LONG).show()
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
-
-    override fun onResume() {
-        super.onResume()
-        if (isMovie)
-            resume()
-    }
-
-    private fun resume() {
-        alphaMovieView!!.onResume()
-        camera = Camera.open()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (isMovie)
-            pause()
-    }
-
-    private fun pause() {
-        alphaMovieView!!.onPause()
-        if (camera != null) {
-            camera!!.setPreviewCallback(null)
-            camera!!.stopPreview()
-            camera!!.release()
-            camera = null
-        }
-    }
-
-    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
-    override fun surfaceCreated(holder: SurfaceHolder) {
-        try {
-            camera!!.setPreviewDisplay(holder)
-            camera!!.setPreviewCallback(this)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        val previewSize = camera!!.parameters.previewSize
-        val aspect = previewSize.width.toFloat() / previewSize.height
-        val previewSurfaceWidth = preview!!.width
-        val previewSurfaceHeight = preview!!.height
-        val lp = preview!!.layoutParams
-
-        // здесь корректируем размер отображаемого preview, чтобы не было искажений
-        if (this.resources.configuration.orientation != Configuration.ORIENTATION_LANDSCAPE) {
-            // портретный вид
-            camera!!.setDisplayOrientation(90)
-            lp.height = previewSurfaceHeight + 100
-            lp.width = ((previewSurfaceHeight + 100)/ aspect).toInt()
-        } else {
-            // ландшафтный
-            camera!!.setDisplayOrientation(0)
-            lp.width = previewSurfaceWidth
-            lp.height = (previewSurfaceWidth / aspect).toInt()
-        }
-        preview!!.layoutParams = lp
-        camera!!.startPreview()
-    }
-
-    override fun surfaceDestroyed(holder: SurfaceHolder) {}
-    override fun onClick(v: View) {
-        if (v === shotBtn) {
-            // либо делаем снимок непосредственно здесь
-            // 	либо включаем обработчик автофокуса
-
-            //camera.takePicture(null, null, null, this);
-            camera!!.autoFocus(this)
-        }
-    }
-
-    override fun onPictureTaken(paramArrayOfByte: ByteArray, paramCamera: Camera) {
-        // сохраняем полученные jpg в папке /sdcard/CameraExample/
-        // имя файла - System.currentTimeMillis()
-        try {
-            val saveDir = File("/sdcard/CameraExample/")
-            if (!saveDir.exists()) {
-                saveDir.mkdirs()
-            }
-            val os = FileOutputStream(String.format("/sdcard/CameraExample/%d.jpg", System.currentTimeMillis()))
-            os.write(paramArrayOfByte)
-            os.close()
-        } catch (e: Exception) {
-        }
-
-        // после того, как снимок сделан, показ превью отключается. необходимо включить его
-        paramCamera.startPreview()
-    }
-
-    override fun onAutoFocus(paramBoolean: Boolean, paramCamera: Camera) {
-        if (paramBoolean) {
-            // если удалось сфокусироваться, делаем снимок
-            paramCamera.takePicture(null, null, null, this)
-        }
-    }
-
-    override fun onPreviewFrame(paramArrayOfByte: ByteArray, paramCamera: Camera) {
-        // здесь можно обрабатывать изображение, показываемое в preview
-    }
-
 }
