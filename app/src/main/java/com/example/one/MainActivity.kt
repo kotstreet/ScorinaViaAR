677351831914749c5804a93b6180a410adffb373
Логинов Антон <loginov.anton.1999@gmail.com>
2021-06-14 13:19:31 +0300

    add new video

--- app/src/main/java/com/example/one/MainActivity.kt
+++ app/src/main/java/com/example/one/MainActivity.kt
@@ -3,17 +3,11 @@ package com.example.one
 import SimpleOrientationListener
 import android.Manifest
 import android.content.pm.PackageManager
-import android.content.res.Configuration
-import android.graphics.drawable.RotateDrawable
-import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import android.util.SparseArray
-import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.View
-import android.view.animation.AnimationUtils
-import android.view.animation.RotateAnimation
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -27,7 +21,7 @@ import com.google.android.gms.vision.barcode.BarcodeDetector
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity :AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private val requestCodeCameraPermission = 1001
     private lateinit var cameraSource: CameraSource
     private lateinit var detector: BarcodeDetector
@@ -41,13 +35,13 @@ class MainActivity :AppCompatActivity() {
 
         alphaMovieView = findViewById(R.id.video_player)
 
-        if(ContextCompat.checkSelfPermission(
+        if (ContextCompat.checkSelfPermission(
                 this@MainActivity,
                 Manifest.permission.CAMERA
-            )!=PackageManager.PERMISSION_GRANTED){
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             askForCameraPermision()
-        }
-        else{
+        } else {
             setUpControlls()
         }
 
@@ -55,10 +49,10 @@ class MainActivity :AppCompatActivity() {
             override fun onSimpleOrientationChanged(orientation: Int) {
                 alphaMovieView!!.post {
                     rotateLayoutView!!.angle =
-                        when(prevOrientation){
-                            1-> 90
-                            2-> 180
-                            3-> 270
+                        when (prevOrientation) {
+                            1 -> 90
+                            2 -> 180
+                            3 -> 270
                             else -> 0
                         }
                 }
@@ -67,7 +61,7 @@ class MainActivity :AppCompatActivity() {
         mOrientationListener.enable()
     }
 
-    private fun setUpControlls(){
+    private fun setUpControlls() {
         detector = BarcodeDetector.Builder(this@MainActivity).build()
         cameraSource = CameraSource.Builder(this@MainActivity, detector)
             .setAutoFocusEnabled(true)
@@ -76,7 +70,7 @@ class MainActivity :AppCompatActivity() {
         detector.setProcessor(processor)
     }
 
-    private fun askForCameraPermision(){
+    private fun askForCameraPermision() {
         ActivityCompat.requestPermissions(
             this@MainActivity,
             arrayOf(Manifest.permission.CAMERA),
@@ -86,17 +80,17 @@ class MainActivity :AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == requestCodeCameraPermission && grantResults.isNotEmpty()){
-            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+        if (requestCode == requestCodeCameraPermission && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 this@MainActivity.recreate()
 //                setUpControlls()
-            }else{
+            } else {
                 Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private val surgaceCallBack = object :SurfaceHolder.Callback{
+    private val surgaceCallBack = object : SurfaceHolder.Callback {
 
         override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
 
@@ -105,53 +99,48 @@ class MainActivity :AppCompatActivity() {
         }
 
         override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
-            try{
-                if(ActivityCompat.checkSelfPermission(
+            try {
+                if (ActivityCompat.checkSelfPermission(
                         this@MainActivity,
                         Manifest.permission.CAMERA
-                    )!=PackageManager.PERMISSION_GRANTED){
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
                     return
                 }
                 cameraSource.start(surfaceHolder)
-            }catch (exception: Exception){
+            } catch (exception: Exception) {
                 Toast.makeText(applicationContext, "Что то пошло не так", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private  val processor = object : Detector.Processor<Barcode>{
+    private val processor = object : Detector.Processor<Barcode> {
         override fun release() {}
 
         override fun receiveDetections(detections: Detector.Detections<Barcode>?) {
-            if(detections!=null && detections.detectedItems.isNotEmpty()
-                && !alphaMovieView!!.isPlaying){
+            if (detections != null && detections.detectedItems.isNotEmpty()
+                && !alphaMovieView!!.isPlaying
+            ) {
                 val qrCodes: SparseArray<Barcode> = detections.detectedItems
                 val code = qrCodes.valueAt(0)
 
-                if(code.displayValue.startsWith("Музей Гомельского государственного университета")) {
+                val path =
+                    when {
+                        code.displayValue.startsWith("Музей Гомельского государственного университета") -> "Videos/Dobro_pozalovat_v_scoriny.mp4"
+                        code.displayValue.startsWith("Книга - это мать") -> "Videos/Kniga_i_poznanie.mp4"
+                        code.displayValue.startsWith("Звери") -> "Videos/Zveri.mp4"
+                        code.displayValue.startsWith("Любите книгу") -> "Videos/Lubite_Knigu.mp4"
+                        code.displayValue.startsWith("Людем посполитым") -> "Videos/ludzam.mp4"
+                        code.displayValue.startsWith("Не копай") -> "Videos/Ne_Kopai.mp4"
+                        else -> ""
+                    }
+
+                if (path != "") {
                     alphaMovieView!!.post() {
                         try {
                             alphaMovieView!!.visibility = View.VISIBLE
-                            alphaMovieView!!.setVideoFromAssets("Videos/Dobro_pozalovat_v_scoriny.mp4")
-//                            alphaMovieView!!.setLooping(false)
-
-//                            alphaMovieView!!.setOnVideoEndedListener {
-//                                alphaMovieView!!.stop()
-//                                alphaMovieView!!.visibility = View.INVISIBLE
-//                            }
-                        } catch (e: Exception) {}
-                    }
-                }else if(code.displayValue.startsWith("Книга - это мать")) {
-                        alphaMovieView!!.post() {
-                            try {
-                                alphaMovieView!!.visibility = View.VISIBLE
-//                                alphaMovieView!!.setLooping(false)
-                                alphaMovieView!!.setVideoFromAssets("Videos/Kniga_i_poznanie.mp4")
-//                                alphaMovieView!!.setOnVideoEndedListener {
-//                                    alphaMovieView!!.stop()
-//                                    alphaMovieView!!.visibility = View.GONE
-//                                }
-                            } catch (e: Exception) {
+                            alphaMovieView!!.setVideoFromAssets(path)
+                        } catch (e: Exception) {
                         }
                     }
                 }
@@ -161,7 +150,7 @@ class MainActivity :AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.d("message","onStart")
+        Log.d("message", "onStart")
 
         alphaMovieView!!.setLooping(false)
         alphaMovieView!!.setOnVideoEndedListener {
@@ -170,37 +159,37 @@ class MainActivity :AppCompatActivity() {
         }
     }
 
-    override fun onStop(){
+    override fun onStop() {
         alphaMovieView!!.visibility = View.INVISIBLE
         super.onStop()
-        Log.d("message","onStop")
+        Log.d("message", "onStop")
     }
 
     override fun onResume() {
         super.onResume()
         alphaMovieView!!.onResume()
-        Log.d("message","onResume")
+        Log.d("message", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
         alphaMovieView!!.onPause()
-        Log.d("message","onPause")
+        Log.d("message", "onPause")
     }
 
     fun play(view: View?) {
 //        alphaMovieView!!.start()
-        Log.d("message","play")
+        Log.d("message", "play")
     }
 
     fun pause(view: View?) {
         alphaMovieView!!.pause()
-        Log.d("message","pause")
+        Log.d("message", "pause")
     }
 
     fun stop(view: View?) {
         alphaMovieView!!.stop()
-        Log.d("message","stop")
+        Log.d("message", "stop")
     }
 
 }
